01a39ae374c392364e15a1f3b74b69724ebe7c24
Habiba Tarek <147947993+habibatarek26@users.noreply.github.com>
2024-10-23 18:16:24 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args)
+    {
+       int size = 10_000_000;
+        int[] randomArray = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            randomArray[i] = random.nextInt();
+        }
+        Selection s =new Selection();
+
+        s.get_median(randomArray);
+        Arrays.sort(randomArray);
+        System.out.print("median by sorting = ");
+        System.out.println(randomArray[5000000]);
+    }
+}
