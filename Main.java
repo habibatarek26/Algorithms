f2ed59fb883c2c85daa220b2332b1b0dd83612fd
habibatarek26 <147947993+habibatarek26@users.noreply.github.com>
2024-10-24 22:10:54 +0200

    .

--- Main.java
+++ Main.java
@@ -4,17 +4,19 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-       int size = 100_000_000;
-        int[]  randomArray=new int[size];
-         Random random = new Random();
+        for (int i=1; i<=8; i++) {
+            int size =(int) Math.pow(10,i);
+            System.out.println("Array size = "+size);
+            int[] randomArray = new int[size];
+            Random random = new Random();
 
-         for (int i = 0; i < size; i++) {
-             randomArray[i] = random.nextInt();
-         }
-        Selection s =new Selection();
+            for (int j = 0; j < size; j++) 
+                randomArray[j] = random.nextInt();
 
-        s.get_median(randomArray);
+            Selection s = new Selection();
 
-
+            s.get_median(randomArray);
+            System.out.println();
+        }
     }
 }
