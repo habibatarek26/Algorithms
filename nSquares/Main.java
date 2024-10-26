61833e84bfcc261e2f4fc7cc8c3d6ef29327f421
habibatarek26 <147947993+habibatarek26@users.noreply.github.com>
2024-10-26 08:54:36 +0200

    .

--- nSquares/Main.java
+++ nSquares/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner s= new Scanner(System.in);
-        String cases = s.next();
+        String cases = "C:\\Users\\matrix\\Documents\\GitHub\\Algorithms\\nSquares\\cases.txt";//s.next();
         Reader r= new Reader();
         System.out.println(r.readTestCases(cases));
 
